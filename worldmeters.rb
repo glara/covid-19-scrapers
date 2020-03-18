79e65e10ecc9f4073618af279344bf4407cc8598
glara <glara@glara.cl>
2020-03-18 19:30:25 +0100

    Fix user agent & table id

--- worldmeters.rb
+++ worldmeters.rb
@@ -1,28 +1,35 @@
+# frozen_string_literal: true
+
 require 'countries/global'
 require 'mechanize'
+require 'byebug'
 
 module Scrappers
   # worldmeters coronavirus scrapper
   class Worldmeters
     TABLE_URL = 'https://www.worldometers.info/coronavirus/'
+    TABLE_NAME = 'main_table_countries_today'
     HEADERS = %i[country total_cases new_cases total_deaths new_deaths
-      total_recovered active_cases serious_critical total_cases_by_1mpop].freeze
+                 total_recovered active_cases serious_critical
+                 total_cases_by_1mpop].freeze
 
     attr_reader :agent
 
     def initialize
       @agent = Mechanize.new
+
+      agent.user_agent = 'Mac Safari'
     end
 
     def summary
       page = agent.get TABLE_URL
-      payload = page.search('table#main_table_countries tbody tr').map do |row|
+      payload = page.search("table##{TABLE_NAME} tbody tr").map do |row|
         next if row.text.include?('Total:')
 
         parse_table_row row
       end
 
-      payload.reject { |item| item.nil? }
+      payload.reject(&:nil?)
     end
 
     private
@@ -33,8 +40,8 @@ module Scrappers
       end).to_h
 
       country = country_by_name record[:country]
-      record[:latitude] = country ? country.latitude : nil
-      record[:longitude] = country ? country.longitude : nil
+      record[:latitude] = country&.latitude
+      record[:longitude] = country&.longitude
 
       record
     end
